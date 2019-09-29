3c15cc0931603270a21dc15981cb9ea81dc47bf5
Yair fernando <yair.facio11@gmail.com>
2019-09-29 07:17:02 -0500

    Create the edit, show and list actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,17 @@
 class ArticlesController < ApplicationController
+    def index
+      @articles = Article.all  
+    end
+
     def new 
       @article = Article.new
     end
 
+    def edit
+      @article = Article.find(params[:id])
+    end
+
+    
     def create
       # render plain: params[:article].inspect
       @article = Article.new(article_params)
@@ -11,6 +20,16 @@ class ArticlesController < ApplicationController
         redirect_to article_path(@article)
       else
         render 'new'
+      end
+    end
+    
+    def update
+      @article = Article.find(params[:id])
+      if @article.update(article_params)
+        flash[:notice] = "Article was successfully updated"
+        redirect_to article_path(@article)
+      else
+        render 'edit'
       end
     end
 
