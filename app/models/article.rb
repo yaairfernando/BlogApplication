94a077f19c65d882b7ae608c202a0e6b673e62ab
Yair fernando <yair.facio11@gmail.com>
2019-10-05 00:31:38 -0500

    Adding gem for encrypting pasword

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,5 @@ class Article < ApplicationRecord
   validates :title, presence: true, length: { minimum: 3, maximum: 50}
   validates :description, presence: true, length: { minimum: 10, maximum: 1000}
   validates :user_id, presence: true
+
 end
