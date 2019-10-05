37c6d0cbfb95f291993e4e8fd283af1ca6358380
Yair fernando <yair.facio11@gmail.com>
2019-10-05 00:55:22 -0500

    Adding user_controller and singup users

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
 
   resources :articles
+
+  get 'signup', to: 'users#new'
+  # post 'users', to: 'users#create'
+  resources :users, except: [:new]
 end
