46430258e77ffcef6f62d4620edba5b481035861
Yair fernando <yair.facio11@gmail.com>
2019-10-10 00:52:35 -0500

    Add article and category association

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
+  has_many :article_categories
+  has_many :articles, through: :article_categories
   validates :name, presence: true, length: { minimum: 3, maximum: 30}
   validates_uniqueness_of :name
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_06_201801) do
+ActiveRecord::Schema.define(version: 2019_10_10_053731) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
